e06266909b4184489b61122d6ae0bacd716b5edb
idrismikayil <idrisrm@code.edu.az>
2022-05-11 21:16:21 +0400

    finish cart page, edit total price

--- assets/js/local-storage.js
+++ assets/js/local-storage.js
@@ -1,12 +1,8 @@
 const localStorage = window.localStorage;
 const cart_btn = document.getElementsByClassName("addToCartBtn")
-// const delete_btn = document.getElementsByClassName("delete")
+const delete_btn = document.getElementsByClassName("delete")
 
 eventListeners();
-
-function Console() {
-    console.log("test-console")
-}
 
 function eventListeners() {
     window.addEventListener("load", function () {
@@ -17,12 +13,7 @@ function eventListeners() {
     })
 }
 
-function Console() {
-    console.log("test-console")
-}
-
 function AddCart(e) {
-
     let id = e.target.name;
     let price = parseFloat(document.getElementById("p-price-" + id).innerText)
     let count = parseInt(document.getElementById("p-count-" + id).value)
@@ -32,7 +23,7 @@ function AddCart(e) {
         product_name: document.getElementById("p-name-" + id).innerText,
         product_price: price,
         product_count: count,
-        total_price: price * count
+        total_price: (price * count).toFixed(2),
     }
 
     AddCartsToStorage(cart);
@@ -54,14 +45,15 @@ function AddCartsToStorage(data) {
     let carts = GetCartsFromStorage();
     carts.push(data);
     localStorage.setItem("carts", JSON.stringify(carts));
+    FillDetailTable();
+    totalPrice();
 }
 
 function DeleteCartFromStorage(id) {
     let carts = GetCartsFromStorage();
-
     carts.splice(id, 1);
-
     localStorage.setItem("carts", JSON.stringify(carts));
+    FillDetailTable();
 }
 
 function ShowAlert() {
@@ -71,113 +63,16 @@ function ShowAlert() {
     }, 2000);
 }
 
-// function GetDetailPage(){
-//     window.location.href='/cart.html';
-// }
-
-// function GetShoppingCartPage(){
-//     window.location.href='/product-detail.html';
-// }
-
-document.getElementsByClassName('selected-products').innerHTML = "<p>+html+</p>";
-
-
 function FillDetailTable() {
     let html = '';
     let carts = GetCartsFromStorage();
 
     for (let i = 0; i < carts.length; i++) {
-        html += '<div class="main">'
-        '<div class="row">'
-        '<div class="col-lg-4 col-md-4 col-sm-12 col-12 product">'
-        '<div class="image">'
-        '<img src=' + carts[i].product_image + ' alt=""/>' +
-            '<div class="title">'
-        '<h5>' + carts[i].product_name + '</h5>' +
-            '</div>'
-        '</div>'
-        '</div>'
-        '<div class="responsive-name  mt-3">'
-        '<h5>' + carts[i].product_name + '</h5>' +
-            '</div>'
-        '<div class="col-lg-2 col-md-2 col-sm-12 col-12 price">'
-        '<h5 class="first-responsive">'
-        '$<span>' + carts[i].product_price + '</span>' +
-            '</h5>'
-        '<h5 class="second-responsive">'
-        'Price: $<span>' + carts[i].product_price + '</span>' +
-            '</h5>'
-        '</div>'
-        '<div class="col-lg-2 col-md-2 col-sm-12 col-12 quantity">'
-        '<h5 class="first-responsive">'
-        '<span>' + carts[i].product_count + '</span>' +
-            '</h5>'
-        '<h5 class="second-responsive">'
-        'Quantity: <span>' + carts[i].product_count + '</span>' +
-            '</h5>'
-        '</div>'
-        '<div class="col-lg-2 col-md-2 col-sm-12 col-12 total">'
-        '<h5 class="first-responsive">'
-        '$<span>' + carts[i].total_price + '</span>' +
-            '</h5>'
-        '<h5 class="second-responsive">'
-        'Total Price: $<span>' + carts[i].total_price + '</span>' +
-            '</h5>'
-        '</div>'
-        '<div class="col-lg-2 col-md-2 col-sm-12 col-12 delete">' +
-        '<i class="fa-solid fa-trash delete" onclick="DeleteCartFromStorage(' + i + ')">' +
-            '</i>'
-        '</div>'
-        '</div>'
-        '</div>'
-
-        // document.getElementsByClassName('selected-products').innerHTML = html;
-        // document.getElementsByClassName('all-price-total').innerText = ?
-        console.log("test")
-        document.getElementsByClassName('selected-products').innerHTML = "<p>+html+</p>";
+        html += '<div class="main"><div class="row"><div class="col-lg-4 col-md-4 col-sm-12 col-12 product"><div class="image"><img src=' + carts[i].product_image + ' alt=""/><div class="title"><h5>' + carts[i].product_name + '</h5></div></div></div><div class="responsive-name  mt-3"><h5>' + carts[i].product_name + '</h5></div><div class="col-lg-2 col-md-2 col-sm-12 col-12 price"><h5 class="first-responsive">$<span>' + carts[i].product_price + '</span></h5><h5 class="second-responsive">Price: $<span>' + carts[i].product_price + '</span> </h5></div><div class="col-lg-2 col-md-2 col-sm-12 col-12 quantity"><h5 class="first-responsive">' + carts[i].product_count + '</h5><h5 class="second-responsive">Quantity: <span>' + carts[i].product_count + '</span></h5></div><div class="col-lg-2 col-md-2 col-sm-12 col-12 total"><h5 class="first-responsive">$<span>' + carts[i].total_price + '</span></h5><h5 class="second-responsive">Total Price: $<span>' + carts[i].total_price + '</span></h5></div><div class="col-lg-2 col-md-2 col-sm-12 col-12 delete"><i class="fa-solid fa-trash delete" onclick="DeleteCartFromStorage(' + i + ')"></i></div></div></div>'
+        document.getElementById('selected-products').innerHTML = html
     }
-}
 
-/* <div class="main">
-<div class="row">
-    <div class="col-lg-4 col-md-4 col-sm-12 col-12 product">
-        <div class="image">
-            <img src="assets/img/avocado-toast.webp" alt=""/>
-            <div class="title">
-                <h5>
-                    Avocado Smash Toast
-                </h5>
-            </div>
-        </div>
-    </div>
-    <div class="responsive-name  mt-3">
-        <h5>
-            Avocado Smash Toast
-        </h5>
-    </div>
-    <div class="col-lg-2 col-md-2 col-sm-12 col-12 price">
-        <h5 class="first-responsive">
-            $<span>
-                5.99
-            </span>
-        </h5>
-        <h5 class="second-responsive">
-            Price: $
-            <span>
-                5.99
-            </span> 
-        </h5>
-    </div>
-    <div class="col-lg-2 col-md-2 col-sm-12 col-12 quantity">
-        <h5 class="first-responsive">1</h5>
-        <h5 class="second-responsive">Quantity: <span>1</span></h5>
-    </div>
-    <div class="col-lg-2 col-md-2 col-sm-12 col-12 total">
-        <h5 class="first-responsive">$<span>5.99</span></h5>
-        <h5 class="second-responsive">Total Price: $<span>5.99</span></h5>
-    </div>
-    <div class="col-lg-2 col-md-2 col-sm-12 col-12 delete">
-        <i class="fa-solid fa-trash"></i>
-    </div>
-</div>
-</div> */
+    let total_all_price = '';
+    total_all_price = '0'
+    document.getElementById('all-price-total').innerText = total_all_price;
+}
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,13 +1,10 @@
 $(document).ready(function () {
-
     $("#products .container .icons .listt").click(function () {
         $(".listt").css("color", "#f34f3f")
         $(".grid").css("color", "#1b1b1b")
         
         $(".grids").removeClass("d-none")
         $(".listss").addClass("d-none")
-
-        
     });
     $("#products .container .icons .grid").click(function () {
         $(".grid").css("color", "#f34f3f")
@@ -15,7 +12,5 @@ $(document).ready(function () {
         
         $(".listss").removeClass("d-none")
         $(".grids").addClass("d-none")
-        
     });
-
 })
